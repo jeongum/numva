a140da14fff48b985d4a551f56474e395f36388a
jeongum <aldlswjddma@naver.com>
2022-03-07 22:40:01 +0900

    init: Spring Boot init

---
+++ server/numva/src/main/java/com/egongil/numva/NumvaApplication.java
@@ -0,0 +1,13 @@
+package com.egongil.numva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumvaApplication.class, args);
+	}
+
+}
