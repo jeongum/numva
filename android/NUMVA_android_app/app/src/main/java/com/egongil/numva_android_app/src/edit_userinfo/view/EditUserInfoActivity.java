9f2594338a886c4775b9f1b6eda48577ff6ffa87
sujin22 <aoghksj@naver.com>
2022-08-31 21:28:29 +0900

    refactor: editUserInfoActivity binding

--- android/NUMVA_android_app/app/src/main/java/com/egongil/numva_android_app/src/edit_userinfo/view/EditUserInfoActivity.java
+++ android/NUMVA_android_app/app/src/main/java/com/egongil/numva_android_app/src/edit_userinfo/view/EditUserInfoActivity.java
@@ -12,63 +12,42 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.databinding.DataBindingUtil;
+
 import com.egongil.numva_android_app.R;
+import com.egongil.numva_android_app.databinding.ActivityEditUserInfoBinding;
 import com.egongil.numva_android_app.src.cert_phone.view.PassActivity;
 import com.egongil.numva_android_app.src.config.models.UserInfo;
-import com.egongil.numva_android_app.src.config.view.BaseActivity;
 import com.egongil.numva_android_app.src.config.models.base.ErrorResponse;
-import com.egongil.numva_android_app.src.custom_dialogs.TwoButtonDialog;
-import com.egongil.numva_android_app.src.edit_userinfo.models.EditUserInfoService;
-import com.egongil.numva_android_app.src.edit_userinfo.interfaces.EditUserInfoActivityContract;
 import com.egongil.numva_android_app.src.config.models.request.EditUserInfoRequest;
 import com.egongil.numva_android_app.src.config.models.response.EditUserInfoResponse;
+import com.egongil.numva_android_app.src.config.view.BaseActivity;
+import com.egongil.numva_android_app.src.custom_dialogs.TwoButtonDialog;
+import com.egongil.numva_android_app.src.edit_userinfo.interfaces.EditUserInfoActivityContract;
+import com.egongil.numva_android_app.src.edit_userinfo.models.EditUserInfoService;
 import com.egongil.numva_android_app.src.main.view.MainActivity;
 import com.egongil.numva_android_app.src.network.ConnectionReceiver;
 import com.egongil.numva_android_app.src.network.NetworkFailureActivity;
 
 import java.util.Random;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-
 public class EditUserInfoActivity extends BaseActivity implements EditUserInfoActivityContract, ConnectionReceiver.ConnectionReceiverListener {
-    UserInfo mUserInfo;
-    TextView mTvName, mTvEmail, mTvNickGuide, mTvDeleteAccount, mTvPhone;
-    EditText mEtBirth, mEtNickname;
-    Button mBtnPass, mBtnRandomNickname, mBtnSave;
-    ImageView  mIvBirthRemoveBtn, mIvNicknameRemoveBtn, mIvExit;
-
+    private UserInfo mUserInfo;
     static final int PASS_EDITUSERINFO_CODE = 999;
 
+    ActivityEditUserInfoBinding binding;
     ActivityResultLauncher<Intent> mActivityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_user_info);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_user_info);
 
         checkConnection(); //네트워크 확인
 
-        mTvName = findViewById(R.id.edituserinfo_tv_username);  //이름
-        mTvEmail = findViewById(R.id.edituserinfo_tv_useremail);  //이메일
-
-        mTvPhone = findViewById(R.id.edituserinfo_et_phone); //핸드폰번호
-        mEtBirth = findViewById(R.id.edituserinfo_et_birth);  //생년월일
-        mEtNickname = findViewById(R.id.edituserinfo_et_nickname);  //닉네임
-
-        mBtnPass = findViewById(R.id.edituserinfo_btn_ctfnumber);  //pass버튼
-        mBtnRandomNickname = findViewById(R.id.edituserinfo_btn_randomnickname);  //랜덤닉네임 버튼
-        mBtnSave = findViewById(R.id.edituserinfo_btn_save);  //저장 버튼
-
-        mIvBirthRemoveBtn = findViewById(R.id.edituserinfo_Iv_birth_remove); //생년월일 x
-        mIvNicknameRemoveBtn = findViewById(R.id.edituserinfo_Iv_nickname_remove);  //닉네임 x
-        mTvNickGuide = findViewById(R.id.edituserinfo_tv_failure_nickname);  //닉네임 fail guide
-
-        mTvDeleteAccount = findViewById(R.id.edituserinfo_tv_delete_account); //회원탈퇴
-
-
-        mIvExit = findViewById(R.id.edituserinfo_iv_crossbtn); //x버튼
-        mIvExit.setOnClickListener(new OnSingleClickListener() {
+        binding.edituserinfoIvCrossbtn.setOnClickListener(new OnSingleClickListener() {
             @Override
             public void onSingleClick(View v) {
                 putIntentUserInfo();
@@ -76,30 +55,32 @@ public class EditUserInfoActivity extends BaseActivity implements EditUserInfoAc
             }
         });
 
+        //Intent에서 UserData 받아오기
         initializeInfo();
 
-        mEtBirth.addTextChangedListener(new TextWatcher() {
+        //생년월일
+        binding.edituserinfoEtBirth.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if(editable.length()>0 && mIvBirthRemoveBtn.getVisibility()==View.GONE){
+                ImageView mIvBirthRemoveBtn = binding.edituserinfoIvBirthRemove;
+                if (editable.length() > 0 && mIvBirthRemoveBtn.getVisibility() == View.GONE) {
                     mIvBirthRemoveBtn.setVisibility(View.VISIBLE);
-                }else if(editable.length()==0 && mIvBirthRemoveBtn.getVisibility()==View.VISIBLE){
+                } else if (editable.length() == 0 && mIvBirthRemoveBtn.getVisibility() == View.VISIBLE) {
                     mIvBirthRemoveBtn.setVisibility(View.GONE);
                 }
             }
         });
 
-        mEtNickname.addTextChangedListener(new TextWatcher() {
+        //닉네임
+        binding.edituserinfoEtNickname.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -112,37 +93,39 @@ public class EditUserInfoActivity extends BaseActivity implements EditUserInfoAc
 
             @Override
             public void afterTextChanged(Editable editable) {
-                if(editable.length()>0 && mIvNicknameRemoveBtn.getVisibility()==View.GONE){
+                ImageView mIvNicknameRemoveBtn = binding.edituserinfoIvNicknameRemove;
+                if (editable.length() > 0 && mIvNicknameRemoveBtn.getVisibility() == View.GONE) {
                     mIvNicknameRemoveBtn.setVisibility(View.VISIBLE);
-                }else if(editable.length()==0 && mIvNicknameRemoveBtn.getVisibility()==View.VISIBLE){
+                } else if (editable.length() == 0 && mIvNicknameRemoveBtn.getVisibility() == View.VISIBLE) {
                     mIvNicknameRemoveBtn.setVisibility(View.GONE);
                 }
             }
         });
 
-        mIvBirthRemoveBtn.setOnClickListener(new OnSingleClickListener() {
+        binding.edituserinfoIvBirthRemove.setOnClickListener(new OnSingleClickListener() {
             @Override
             public void onSingleClick(View v) {
-                mEtBirth.setText("");
+                binding.edituserinfoEtBirth.setText("");
             }
         });
 
-        mIvNicknameRemoveBtn.setOnClickListener(new OnSingleClickListener() {
+        binding.edituserinfoIvNicknameRemove.setOnClickListener(new OnSingleClickListener() {
             @Override
             public void onSingleClick(View v) {
-                mEtNickname.setText("");
+                binding.edituserinfoEtNickname.setText("");
             }
         });
 
-        mBtnPass.setOnClickListener(v -> {
+        //PASS버튼
+        binding.edituserinfoBtnCtfnumber.setOnClickListener(v -> {
             Intent intent = new Intent(EditUserInfoActivity.this, PassActivity.class);
             intent.putExtra("request_page", "edituserinfo");
             mActivityResultLauncher.launch(intent);
         });
 
-        mBtnRandomNickname.setOnClickListener(v -> mEtNickname.setText(setRandNick()));
+        binding.edituserinfoBtnRandomnickname.setOnClickListener(v -> binding.edituserinfoEtNickname.setText(setRandNick()));
 
-        mTvDeleteAccount.setOnClickListener(new OnSingleClickListener() {
+        binding.edituserinfoTvDeleteAccount.setOnClickListener(new OnSingleClickListener() {
             @Override
             public void onSingleClick(View v) {
                 TwoButtonDialog deleteAccountDialog = new TwoButtonDialog(EditUserInfoActivity.this);
@@ -165,7 +148,7 @@ public class EditUserInfoActivity extends BaseActivity implements EditUserInfoAc
             }
         });
 
-        mBtnSave.setOnClickListener(new OnSingleClickListener() {
+        binding.edituserinfoBtnSave.setOnClickListener(new OnSingleClickListener() {
             @Override
             public void onSingleClick(View v) {
                 editUserInfo();
@@ -180,40 +163,38 @@ public class EditUserInfoActivity extends BaseActivity implements EditUserInfoAc
                 String strPhone = intent.getStringExtra("phone");
                 String strBirth = intent.getStringExtra("birth");
 
-                mTvName.setText(strName);
-                mTvPhone.setText(strPhone);
-                mEtBirth.setText(strBirth);
-
                 mUserInfo.setName(strName);
                 mUserInfo.setPhone(strPhone);
                 mUserInfo.setBirth(strBirth);
-                //TODO: ViewModel 구현하여 양방향 binding
+
+                setUserData();
             }
         });
     }
 
-    private void initializeInfo(){
+    private void initializeInfo() {
         Intent intent = getIntent();
-        mUserInfo = (UserInfo)intent.getSerializableExtra("user_info");
+        mUserInfo = (UserInfo) intent.getSerializableExtra("user_info");
+        setUserData();
+    }
 
-        //TODO: ViewModel 구현하여 양방향 바인딩
-        mTvName.setText(mUserInfo.getName());
-        mTvEmail.setText(mUserInfo.getEmail());
-        mEtBirth.setText(mUserInfo.getBirth());
-        mEtNickname.setText(mUserInfo.getNickname());
-        mTvPhone.setText(mUserInfo.getPhone());
+    private void setUserData() {
+        binding.edituserinfoTvUsername.setText(mUserInfo.getName());
+        binding.edituserinfoTvUseremail.setText(mUserInfo.getEmail());
+        binding.edituserinfoEtBirth.setText(mUserInfo.getBirth());
+        binding.edituserinfoEtNickname.setText(mUserInfo.getNickname());
+        binding.edituserinfoEtPhone.setText(mUserInfo.getPhone());
     }
 
     @Override
     public void postEditUserInfoSuccess(EditUserInfoResponse editUserInfoResponse, ErrorResponse errorResponse) {
-        if(editUserInfoResponse!=null){
-            if(editUserInfoResponse.getCode()==200 && editUserInfoResponse.isSuccess()){
+        if (editUserInfoResponse != null) {
+            if (editUserInfoResponse.getCode() == 200 && editUserInfoResponse.isSuccess()) {
                 showCustomToast("회원정보 수정 완료되었습니다.");
                 putIntentUserInfo();//finish_intent에 userData 담기
                 finish();
             }
-        }
-        else if(errorResponse!=null){
+        } else if (errorResponse != null) {
         }
     }
 
@@ -222,54 +203,56 @@ public class EditUserInfoActivity extends BaseActivity implements EditUserInfoAc
 
     }
 
-    private void editUserInfo(){
-        mUserInfo.setNickname(mEtNickname.getText().toString());
+    private void editUserInfo() {
+        mUserInfo.setNickname(binding.edituserinfoEtNickname.getText().toString());
+        mUserInfo.setBirth(binding.edituserinfoEtBirth.getText().toString());
 
         EditUserInfoService editUserInfoService = new EditUserInfoService(this);
         EditUserInfoRequest editUserInfoRequest = new EditUserInfoRequest();
         editUserInfoRequest.setPhone(mUserInfo.getPhone());
         editUserInfoRequest.setBirth(mUserInfo.getBirth());
-        editUserInfoRequest.setNickname(mEtNickname.getText().toString());
+        editUserInfoRequest.setNickname(binding.edituserinfoEtNickname.getText().toString());
 
         editUserInfoService.postEditUserInfo(editUserInfoRequest);
     }
+
     //랜덤닉네임 메소드
-    private String setRandNick(){
+    private String setRandNick() {
         Random rand = new Random();
         int n, m;
         String strnick1, strnick2;
 
         String[] nick1 = getResources().getStringArray(R.array.nick1);
-        n = rand.nextInt(nick1.length-1);
+        n = rand.nextInt(nick1.length - 1);
         strnick1 = nick1[n];
 
         String[] nick2 = getResources().getStringArray(R.array.nick2);
-        m = rand.nextInt(nick2.length-1);
+        m = rand.nextInt(nick2.length - 1);
         strnick2 = nick2[m];
 
-        return strnick1+strnick2;
+        return strnick1 + strnick2;
     }
 
     @Override
     public void onNetworkConnectionChanged(boolean isConnected) {
-        if(!isConnected){
+        if (!isConnected) {
             Intent intent = new Intent(this, NetworkFailureActivity.class);
             startActivity(intent);
-        }else{
+        } else {
 
         }
     }
 
-    private void checkConnection(){
+    private void checkConnection() {
         boolean isConnected = ConnectionReceiver.isConnected();
-        if(!isConnected){
+        if (!isConnected) {
             Intent intent = new Intent(this, NetworkFailureActivity.class);
             startActivity(intent);
         }
     }
 
     //MyPageFragment 향할 intent에 userData 담기
-    public void putIntentUserInfo(){
+    public void putIntentUserInfo() {
         Intent finish_intent = new Intent(getApplicationContext(), MainActivity.class);
         finish_intent.putExtra("user_info", mUserInfo);
         setResult(EDIT_USERINFO_ACTIVITY, finish_intent);
